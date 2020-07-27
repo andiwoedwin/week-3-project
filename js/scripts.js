d68e6bec7285b0a6334ba473bcf52bdf329250ee
andiwoedwin <andiwoedwin@gmail.com>
2020-07-27 04:35:26 +0300

    add js to our website

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#design-img").click(function(){
+      $("#design-img").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-img").slideDown('1500');
+    });
+  });
